aa6a5e5cc48cc566bab37e43e72ed843113719bb
Kirill Gavrilov <kirill@sview.ru>
2025-01-13 21:32:53 +0300

    FFmpeg-7.1

--- libavutil/avutil.cpp
+++ libavutil/avutil.cpp
@@ -129,3 +129,57 @@ FFEXPORT void    av_frame_set_channel_layout(AVFrame* frame, int64_t val) FFBODY
 FFEXPORT int     av_frame_get_channels(const AVFrame* frame) FFBODYNULL
 FFEXPORT void    av_frame_set_channels(AVFrame* frame, int     val) FFBODYVOID
 FFEXPORT int     av_channel_layout_describe(const AVChannelLayout *channel_layout, char *buf, size_t buf_size) FFBODYNULL
+
+// options API
+FFEXPORT int av_set_options_string(void *ctx, const char *opts, const char *key_val_sep, const char *pairs_sep) FFBODYNULL
+FFEXPORT int av_opt_set_from_string(void *ctx, const char *opts, const char *const *shorthand, const char *key_val_sep, const char *pairs_sep) FFBODYNULL
+FFEXPORT int av_opt_set_dict(void *obj, struct AVDictionary **options) FFBODYNULL
+FFEXPORT int av_opt_set_dict2(void *obj, struct AVDictionary **options, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_copy(void *dest, const void *src) FFBODYNULL
+FFEXPORT int av_opt_set         (void *obj, const char *name, const char *val, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_int     (void *obj, const char *name, int64_t     val, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_double  (void *obj, const char *name, double      val, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_q       (void *obj, const char *name, AVRational  val, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_bin     (void *obj, const char *name, const uint8_t *val, int size, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_image_size(void *obj, const char *name, int w, int h, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_pixel_fmt (void *obj, const char *name, enum AVPixelFormat fmt, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_sample_fmt(void *obj, const char *name, enum AVSampleFormat fmt, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_video_rate(void *obj, const char *name, AVRational val, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_chlayout(void *obj, const char *name, const AVChannelLayout *layout, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_dict_val(void *obj, const char *name, const AVDictionary *val, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_set_array(void *obj, const char *name, int search_flags, unsigned int start_elem, unsigned int nb_elems, enum AVOptionType val_type, const void *val) FFBODYNULL
+
+FFEXPORT int av_opt_get         (void *obj, const char *name, int search_flags, uint8_t   **out_val) FFBODYNULL
+FFEXPORT int av_opt_get_int     (void *obj, const char *name, int search_flags, int64_t    *out_val) FFBODYNULL
+FFEXPORT int av_opt_get_double  (void *obj, const char *name, int search_flags, double     *out_val) FFBODYNULL
+FFEXPORT int av_opt_get_q       (void *obj, const char *name, int search_flags, AVRational *out_val) FFBODYNULL
+FFEXPORT int av_opt_get_image_size(void *obj, const char *name, int search_flags, int *w_out, int *h_out) FFBODYNULL
+FFEXPORT int av_opt_get_pixel_fmt (void *obj, const char *name, int search_flags, enum AVPixelFormat *out_fmt) FFBODYNULL
+FFEXPORT int av_opt_get_sample_fmt(void *obj, const char *name, int search_flags, enum AVSampleFormat *out_fmt) FFBODYNULL
+FFEXPORT int av_opt_get_video_rate(void *obj, const char *name, int search_flags, AVRational *out_val) FFBODYNULL
+FFEXPORT int av_opt_get_chlayout(void *obj, const char *name, int search_flags, AVChannelLayout *layout) FFBODYNULL
+FFEXPORT int av_opt_get_dict_val(void *obj, const char *name, int search_flags, AVDictionary **out_val) FFBODYNULL
+FFEXPORT int av_opt_get_array_size(void *obj, const char *name, int search_flags, unsigned int *out_val) FFBODYNULL
+FFEXPORT int av_opt_get_array(void *obj, const char *name, int search_flags, unsigned int start_elem, unsigned int nb_elems, enum AVOptionType out_type, void *out_val) FFBODYNULL
+
+struct AVOption;
+FFEXPORT int av_opt_eval_flags (void *obj, const AVOption *o, const char *val, int        *flags_out) FFBODYNULL
+FFEXPORT int av_opt_eval_int   (void *obj, const AVOption *o, const char *val, int        *int_out) FFBODYNULL
+FFEXPORT int av_opt_eval_uint  (void *obj, const AVOption *o, const char *val, unsigned   *uint_out) FFBODYNULL
+FFEXPORT int av_opt_eval_int64 (void *obj, const AVOption *o, const char *val, int64_t    *int64_out) FFBODYNULL
+FFEXPORT int av_opt_eval_float (void *obj, const AVOption *o, const char *val, float      *float_out) FFBODYNULL
+FFEXPORT int av_opt_eval_double(void *obj, const AVOption *o, const char *val, double     *double_out) FFBODYNULL
+FFEXPORT int av_opt_eval_q     (void *obj, const AVOption *o, const char *val, AVRational *q_out) FFBODYNULL
+
+struct AVClass;
+FFEXPORT void *av_opt_ptr(const AVClass *avclass, void *obj, const char *name) FFBODYNULL
+
+FFEXPORT int av_opt_is_set_to_default(void *obj, const AVOption *o) FFBODYNULL
+FFEXPORT int av_opt_is_set_to_default_by_name(void *obj, const char *name, int search_flags) FFBODYNULL
+FFEXPORT int av_opt_flag_is_set(void *obj, const char *field_name, const char *flag_name) FFBODYNULL
+FFEXPORT int av_opt_serialize(void *obj, int opt_flags, int flags, char **buffer, const char key_val_sep, const char pairs_sep) FFBODYNULL
+
+struct AVOptionRanges;
+FFEXPORT void av_opt_freep_ranges(AVOptionRanges **ranges) FFBODYVOID
+FFEXPORT int av_opt_query_ranges(AVOptionRanges **, void *obj, const char *key, int flags) FFBODYNULL
+FFEXPORT int av_opt_query_ranges_default(AVOptionRanges **, void *obj, const char *key, int flags) FFBODYNULL
